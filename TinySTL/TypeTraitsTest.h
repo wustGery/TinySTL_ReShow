75fc53f4bce56958f30978527bde04bc5c24fe63
wustGery <24583145078@qq.com>
2018-09-06 19:15:57 +0800

    类型萃取测试头文件

---
+++ TinySTL/TypeTraitsTest.h
@@ -0,0 +1,18 @@
+//
+// Created by wust_gery on 18-7-26.
+//
+
+#ifndef TINYSTL_TYPETRAITSTEST_H
+#define TINYSTL_TYPETRAITSTEST_H
+
+
+#include "TypeTraits.h"
+
+namespace TinySTL
+{
+	namespace TypeTraitsTest
+	{
+		void testAllCases();
+	}
+}
+#endif //TINYSTL_TYPETRAITSTEST_H
