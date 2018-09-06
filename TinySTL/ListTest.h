16741fab9cb7d937bd4722004263698d3bff3623
wustGery <24583145078@qq.com>
2018-09-06 19:13:59 +0800

    List测试头文件

---
+++ TinySTL/ListTest.h
@@ -0,0 +1,17 @@
+#include "List.h"
+
+namespace TinySTL
+{
+	namespace ListTest
+	{
+		template<class T>
+		using tsLst = TinySTL::list<T>;
+
+		void testCase1();
+		void testCase2();
+
+		void testAllCases();
+	}
+}
+
+
