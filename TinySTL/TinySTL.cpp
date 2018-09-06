bfd0f542ee0893aa81ae883964dbecffcfb5575d
wustGery <24583145078@qq.com>
2018-09-06 19:19:47 +0800

    容器测试入口

---
+++ TinySTL/TinySTL.cpp
@@ -0,0 +1,18 @@
+// TinySTL.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "TypeTraitsTest.h"
+//#include "VectorTest.h"
+#include "ListTest.h"
+#include<iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//TinySTL::TypeTraitsTest::testAllCases();
+	//TinySTL::VectorTest::testAllCases();
+	TinySTL::ListTest::testAllCases();
+	return 0;
+}
+
