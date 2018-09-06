b92ef35f4cbabbc8dd96f91caf4873c74b1d7545
wustGery <24583145078@qq.com>
2018-09-06 19:20:42 +0800

    类型萃取测试实现

---
+++ TinySTL/TypeTraitsTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by wust_gery on 18-7-26.
+//
+#include "stdafx.h"
+#include "TypeTraitsTest.h"
+#include <cassert>
+#include<iostream>
+
+using namespace std;
+
+
+namespace TinySTL
+{
+	namespace TypeTraitsTest
+	{
+		void testCase1()
+		{
+			std::cout<< "hello" << std::endl;
+		}
+
+		void testAllCases()
+		{
+			testCase1();
+		}
+	}
+}
